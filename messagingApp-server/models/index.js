8ce9fe3e1dd9de632c80355d559c86e35f50eeb5
VishwanaOnGit <putchavishwanath@gmail.com>
2024-02-23 22:03:30 +0530

    exported users and message

--- messagingApp-server/models/index.js
+++ messagingApp-server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/messagingApp-server", {
-  keepAlive: true,
-  useMongoClient: true,
-});
+mongoose.connect("mongodb://localhost/messagingApp-server");
+
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
