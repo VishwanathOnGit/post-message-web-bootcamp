dd6c8c581e3448040e55b97a1d460065c1940e0a
VishwanaOnGit <putchavishwanath@gmail.com>
2024-02-23 21:22:10 +0530

    Added bCrypt logic

--- messagingApp-server/models/user.js
+++ messagingApp-server/models/user.js
@@ -1,26 +1,39 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profileImageUrl: {
-        type: String
-    }
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profileImageUrl: {
+    type: String,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
