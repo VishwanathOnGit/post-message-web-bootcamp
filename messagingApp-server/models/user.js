8cb5c69cf10cc75af9491f450a26064f8082542c
VishwanaOnGit <putchavishwanath@gmail.com>
2024-02-23 21:30:16 +0530

    Added compare password logic

--- messagingApp-server/models/user.js
+++ messagingApp-server/models/user.js
@@ -34,6 +34,15 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+userSchema.methods.comparePassword = async function (password, next) {
+  try {
+    let isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
