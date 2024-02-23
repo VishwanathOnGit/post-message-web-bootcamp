23215f9680f41ddffbb82d72615d7c16109ff07b
VishwanaOnGit <putchavishwanath@gmail.com>
2024-02-23 21:16:28 +0530

    Added User schema

---
+++ messagingApp-server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImageUrl: {
+        type: String
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
