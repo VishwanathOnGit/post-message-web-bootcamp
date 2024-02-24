fc819cdbd01f5685512f56f65418870a4524de43
VishwanaOnGit <putchavishwanath@gmail.com>
2024-02-24 21:01:18 +0530

    Add in-memory mongoDB

--- messagingApp-server/models/index.js
+++ messagingApp-server/models/index.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/messagingApp-server");
+
+async function startInMemoryMongoDB() {
+  const mongod = new MongoMemoryServer();
+  const uri = await mongod.getUri();
+  console.log("In-memory MongoDB URI:", uri);
+  mongoose.connect(uri);
+}
+
+startInMemoryMongoDB();
 
 module.exports.User = require("./user");
 module.exports.Message = require("./message");
